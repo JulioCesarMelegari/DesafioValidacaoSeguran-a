7329fdab805d637db8dfe3b617e887cebfd827a4
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-08-27 10:23:56 -0300

    add validações, add endPoint get cities

---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.bds04.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		List<City> result = repository.findAll();
+		return result.stream().map(x -> new CityDTO(x)).toList();
+	}
+	
+	
+
+}

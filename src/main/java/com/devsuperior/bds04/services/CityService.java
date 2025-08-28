6ec1ab4a081297476c60f508a60a973ab3abb692
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-08-28 12:04:48 -0300

    corrigido endPoints cities e events

--- src/main/java/com/devsuperior/bds04/services/CityService.java
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds04.dto.CityDTO;
 import com.devsuperior.bds04.entities.City;
 import com.devsuperior.bds04.repositories.CityRepository;
+import com.devsuperior.bds04.services.exceptions.DatabaseException;
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CityService {
@@ -18,8 +23,15 @@ public class CityService {
 	private CityRepository repository;
 	
 	@Transactional(readOnly = true)
+	public CityDTO findById(Long id){
+		City result = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Cidade não cadastrada"));
+		return new CityDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAll(){
-		List<City> result = repository.listAllOrderByName();
+		List<City> result = repository.findAll(Sort.by("name"));
 		return result.stream().map(x -> new CityDTO(x)).toList();
 	}
 	
@@ -30,18 +42,16 @@ public class CityService {
 		entity = repository.save(entity);
 		return new CityDTO(entity);
 	}
-	/*
-    @Transactional
-    public CityDTO update(Long id, CityDTO dto) {
-        try {
-        	City entity = repository.getReferenceById(id);
-        	BeanUtils.copyProperties(dto, entity);
-            entity = repository.save(entity);
-            return new CityDTO(entity);
-        }
-        catch (EntityNotFoundException e) {
-            throw new ResourceNotFoundException("Recurso não encontrado");
-        }
-    }
-	*/
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Cidade não cadastrada");
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Falha de integridade referencial");
+		}
+	}
 }
--- src/main/java/com/devsuperior/bds04/services/EventService.java
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -8,28 +8,59 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
 import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
 import com.devsuperior.bds04.repositories.EventRepository;
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class EventService {
-	
+
 	@Autowired
-	private EventRepository repository;
-	
+	private EventRepository eventRepository;
+
+	@Autowired
+	private CityRepository cityRepository;
+
 	@Transactional(readOnly = true)
-	 public Page<EventDTO> findAll(Pageable pageable) {
-		Page<Event> result = repository.findAll(pageable);
+	public Page<EventDTO> findAll(Pageable pageable) {
+		Page<Event> result = eventRepository.findAll(pageable);
 		return result.map(x -> new EventDTO(x));
 	}
-	
+
 	@Transactional
 	public EventDTO insert(EventDTO dto) {
 		Event entity = new Event();
-		//corrigir metodo : buscar o id da cidade no repository, se for nulo erro 422
-		BeanUtils.copyProperties(dto, entity);
-		entity = repository.save(entity);
-		return new EventDTO(entity);
+		if (!cityRepository.existsById(dto.getCityId())) {
+			throw new ResourceNotFoundException("Cidade não cadastrada");
+		} else {
+			BeanUtils.copyProperties(dto, entity);
+			City city = cityRepository.findById(dto.getCityId()).get();
+			entity.setCity(city);
+			entity = eventRepository.save(entity);
+			return new EventDTO(entity);
+		}
 	}
-	
+
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Event entity = eventRepository.getReferenceById(id);
+			City city = cityRepository.findById(dto.getCityId())
+					.orElseThrow(() -> new ResourceNotFoundException("Cidade não encontrada"));
+			entity.setCity(city);
+			entity.setDate(dto.getDate());
+			entity.setId(id);
+			entity.setName(dto.getName());
+			entity.setUrl(dto.getUrl());
+			entity = eventRepository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Evento não encontrado");
+		}
+	}
+
 }
